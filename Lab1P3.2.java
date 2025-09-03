efb1e38644084a5cadcd4ef291991810de15eec0
yash-d7 <yashvardhan.durgam@gmail.com>
2025-09-03 12:31:24 +0530

    Add files via upload

---
+++ Lab1P3.2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Lab1P3{
+	public static void main(String...args){
+		Scanner s = new Scanner(System.in);
+		try{
+		int a = s.nextInt();
+		int b = s.nextInt();
+		var c = a/b;
+		System.out.println(c);
+		}catch(Exception e){
+		
+      		System.out.println(e.getClass());
+    	}
+    }
+}
