2bd16d82ea37329bfa6e34e53e811a02f2da3113
yash-d7 <yashvardhan.durgam@gmail.com>
2025-09-03 11:31:40 +0530

    Add files via upload

---
+++ Lab1P2.java
@@ -0,0 +1,79 @@
+class Lab1P2{
+	public static void main(String..args){
+		byte b = 127;	
+		Byte bb = b; 
+		
+		
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(bb.SIZE);
+		System.out.println(bb.TYPE);
+		System.out.println(bb.BYTES);
+		System.out.println("-------SHORT-------");
+		
+		short s = 18;
+		short ss = s;  //AutoBoxing
+		
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.TYPE);
+		System.out.println(ss.BYTES);
+		System.out.println("-------INTEGER-------");
+		
+		int x = 11;
+		Integer xx = x;
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(xx.MIN_VALUE);
+		System.out.println(xx.SIZE);
+		System.out.println(xx.TYPE);
+		System.out.println(xx.BYTES);
+		System.out.println("-------LONG-------");
+		
+		long y = 28;
+		Long yy = y;
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(yy.MIN_VALUE);
+		System.out.println(yy.SIZE);
+		System.out.println(yy.TYPE);
+		System.out.println(yy.BYTES);
+		System.out.println("-------FLOAT-------");
+		
+		float p = 30;
+		Float pp = p;
+		
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(pp.MIN_VALUE);
+		System.out.println(pp.SIZE);
+		System.out.println(pp.TYPE);
+		System.out.println(pp.BYTES);
+		System.out.println("-------DOUBLE-------");
+		
+		double d = 7;
+		Double dd = d;
+		
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(dd.MIN_VALUE);
+		System.out.println(dd.SIZE);
+		System.out.println(dd.TYPE);
+		System.out.println(dd.BYTES);
+		System.out.println("-------BOOLEAN-------");
+		
+		boolean b = 8;
+		Boolean bb = b;
+		
+		System.out.println(Boolean.MAX_VALUE);
+		System.out.println(dd.MIN_VALUE);
+		System.out.println(dd.SIZE);
+		System.out.println(dd.TYPE);
+		System.out.println(dd.BYTES);
+		System.out.println("--------------------");	
+		
+		
+		
+	}
+		
+}	
