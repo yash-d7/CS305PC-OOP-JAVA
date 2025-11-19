dbfcb5e37eb6c3391fae9c824366f41aabe38172
yash-d7 <yashvardhan.durgam@gmail.com>
2025-11-19 18:35:50 +0530

    Update Lab3P6.java

--- LAB 3/Lab3P6.java
+++ LAB 3/Lab3P6.java
@@ -1,8 +1,8 @@
-class Lab3P52 extends Lab3P6{
+class Lab3P6 extends Lab3P5{
 	public void demonstrate(){
 		System.out.println("I am a method in Lab3P51");	
 	}
-	Lab3P52(){
+	Lab3P6(){
 		super.demonstrate();
 	}
 }
