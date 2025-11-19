7e55285655c23cc5c61ef264987b6f47f2d02e49
yash-d7 <yashvardhan.durgam@gmail.com>
2025-11-19 18:39:21 +0530

    Create Lab5P3.java

---
+++ LAB 5/Lab5P3.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Add{
+    public void add(int...a){
+    String str = " "; 
+    int sum = 0;
+    int count = 0;
+        for(int i:a){
+        sum = sum +i;
+        System.out.print(i);
+        if(count!=a.length-1){
+        System.out.print("+");
+        }count++;
+        }
+        System.out.print("=" +sum+"\n");
+    }
+    
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	
+	
