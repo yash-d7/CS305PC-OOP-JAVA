c4aa1092c9868e50cfcb106527ba79f714135bb5
yash-d7 <yashvardhan.durgam@gmail.com>
2025-11-19 18:32:58 +0530

    Create Lab3P5.java

---
+++ LAB 3/Lab3P5.java
@@ -0,0 +1,12 @@
+class Lab3P51{
+	String name;
+	public void demonstrate(){
+		System.out.println("I am a method in Lab3P51");	
+	}
+	public void setName(String s){
+		this.name = s;
+	}
+	public String getName(){
+		return this.name;
+	}
+}
